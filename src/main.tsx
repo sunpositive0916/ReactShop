c4f0794a186d85bc8721f09eba10b8a9018d2089
MOON <sunpositive7@gmail.com>
2022-06-17 22:45:46 +0900

    라우터 처리, 상품목록, 상세페이지, GNB 완성

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./scss/index.scss";
 import App from "./app";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
