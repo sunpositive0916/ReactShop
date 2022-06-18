3d857af18b43748cf29942414553081a5ca40b94
MOON <sunpositive7@gmail.com>
2022-06-18 19:40:18 +0900

    MSW를 이용한 상품리스트 불러오기 / 장바구니 추가

--- src/queryClient.ts
+++ src/queryClient.ts
@@ -1,3 +1,4 @@
+import { request, RequestDocument } from "graphql-request";
 import { QueryClient } from "react-query";
 
 // import { getTodos, postTodo } from '../my-api'
@@ -23,9 +24,9 @@ export const getClient = (() => {
   };
 })();
 
-const BASE_URL = "https://fakestoreapi.com";
+const BASE_URL = "/";
 
-export const fetcher = async ({
+export const restfetcher = async ({
   method,
   path,
   body,
@@ -59,6 +60,10 @@ export const fetcher = async ({
   }
 };
 
+export const graphqlFetcher = (query: RequestDocument, variables = {}) =>
+  request(BASE_URL, query, variables);
+
 export const QueryKeys = {
   PRODUCTS: "PRODUCTS",
+  CART: "CART",
 };
