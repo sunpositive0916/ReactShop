71dc02d2ffdfc42c40ef3760f939adee08b1e0da
MOON <sunpositive7@gmail.com>
2022-06-18 23:17:53 +0900

    장바구니 추가, 삭제 전체선택 및 해제 기능 추가

--- src/queryClient.ts
+++ src/queryClient.ts
@@ -1,8 +1,6 @@
 import { request, RequestDocument } from "graphql-request";
 import { QueryClient } from "react-query";
 
-// import { getTodos, postTodo } from '../my-api'
-
 type AnyOBJ = { [key: string]: any };
 
 export const getClient = (() => {
@@ -12,8 +10,8 @@ export const getClient = (() => {
       client = new QueryClient({
         defaultOptions: {
           queries: {
-            cacheTime: 1000 * 60 * 60 * 24,
-            staleTime: 1000 * 60,
+            cacheTime: Infinity,
+            staleTime: Infinity,
             refetchOnMount: false,
             refetchOnReconnect: false,
             refetchOnWindowFocus: false,
